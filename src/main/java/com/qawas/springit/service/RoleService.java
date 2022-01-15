e9a3ec93e26ecb21b40ba8c99e284fa6ba572b08
abdallah-nazzal <abdallahqawas@gmail.com>
2022-01-15 20:09:50 +0200

    User registration

---
+++ src/main/java/com/qawas/springit/service/RoleService.java
@@ -0,0 +1,19 @@
+package com.qawas.springit.service;
+
+import com.qawas.springit.domain.Role;
+import com.qawas.springit.repository.RoleRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleService {
+
+    private final RoleRepository roleRepository;
+
+    public RoleService(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    public Role findByName(String name) {
+        return roleRepository.findByName(name);
+    }
+}
